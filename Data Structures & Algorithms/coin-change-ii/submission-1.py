4d3ad158e4b4c405176f8cdb7c227e36cbecbeef
Sam Wang <wang.sam33@gmail.com>
2026-06-27 19:42:35 -0700

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        '''
+        dp[i] = sum dp[i-c] for c in coins
+        '''
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for c in coins:
+            for i in range(c, amount+1):
+                dp[i] += dp[i-c]
+        return dp[-1]
