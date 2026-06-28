e1f79a2725fd232b3d2159c79e9bc4e96ad3d2f8
Sam Wang <wang.sam33@gmail.com>
2026-06-27 22:11:02 -0700

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,17 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x: x.start)
+        prevEnd = float('-inf')
+        for interval in intervals:
+            if interval.start < prevEnd:
+                return False
+            prevEnd = interval.end
+        return True
